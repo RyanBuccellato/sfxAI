b9d874bda70b7d921e0e7a7262a0ea3c51e1b679
abrutalcyclone <ryanbuccellato@gmail.com>
2023-04-25 02:08:16 -0700

    finally working locally but aint no way this working in vercel

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
-from http.server import BaseHTTPRequestHandler
-from urllib.parse import urlparse, parse_qs
+import io
+from flask import Flask, make_response, request, Response, send_file
 
 import numpy as np
+from scipy.io.wavfile import write
 from audioldm import text_to_audio, build_model
+
+app = Flask(__name__)
 
 audioldm = None
 current_model_name = None
@@ -14,8 +17,7 @@ def text2audio(text, duration, guidance_scale, random_seed, n_candidates, model_
         audioldm=build_model(model_name=model_name)
         current_model_name = model_name
         
-    # print(text, length, guidance_scale)
-    waveform = text_to_audio(
+    return text_to_audio(
         latent_diffusion=audioldm,
         text=text,
         seed=random_seed,
@@ -24,16 +26,16 @@ def text2audio(text, duration, guidance_scale, random_seed, n_candidates, model_
         n_candidate_gen_per_text=int(n_candidates),
     )  # [bs, 1, samples]
  
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        parsed_url = urlparse(self.path)
-        query_params = parse_qs(parsed_url.query)
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        prompt = query_params.get("prompt", [""])[0]
-        duration = float(query_params.get("duration", ["5"])[0])
-        candidates = int(query_params.get("candidates", ["3"])[0])
-        waveform = text2audio(prompt, duration, 2.5, 123, candidates, "audioldm-s-full-v2")
-        self.wfile.write(waveform)
-        return
+@app.route('/')
+def query():
+    prompt = request.args.get("prompt", "")
+    duration = float(request.args.get("duration", "5"))
+    candidates = int(request.args.get("candidates", "3"))
+    wav = text2audio(prompt, duration, 2.5, 123, candidates, "audioldm-s-full")
+    bytes_wav = bytes()
+    byte_io = io.BytesIO(bytes_wav)
+    write(byte_io, 16000, wav)
+    headers = {
+        f'Content-Disposition': f'attachment; filename={prompt}.wav'
+    }
+    return Response(byte_io.read(), mimetype="audio/wav", headers=headers)
