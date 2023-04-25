0dfc940880f2339edadf4c5d83f7ab9cb6f7cc65
abrutalcyclone <ryanbuccellato@gmail.com>
2023-04-24 23:28:05 -0700

    v1

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler
+import urlparse
+
+import audioldm
  
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+        o = urlparse.urlparse(self.path)
+        q = urlparse.parse_qs(o.query)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(f'{q.get("q")}}'.encode('utf-8'))
         return
