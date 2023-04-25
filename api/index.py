0aeda936a9c7815332e7d3f1d040ed3dc6d5ed01
abrutalcyclone <ryanbuccellato@gmail.com>
2023-04-24 23:35:35 -0700

    gpt 2 save day

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
 from http.server import BaseHTTPRequestHandler
-import urlparse
+from urllib.parse import urlparse, parse_qs
 
 #import audioldm
  
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
-        o = urlparse.urlparse(self.path)
-        q = urlparse.parse_qs(o.query)
+        parsed_url = urlparse(self.path)
+        query_params = parse_qs(parsed_url.query)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
